0f3ad0c6f3823b26b3d3f2350d7748f78a08517a
HossamMahmoudKhedr <nhem318@gmail.com>
2024-03-05 11:38:03 +0200

    add new projects:

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -2,10 +2,21 @@ import { icons } from '../utils/icons';
 
 export const projData = [
 	{
+		id: 13,
+		name: 'Waiter Dashboard',
+		describtion:
+			'The Dashboard is an innovative platform empowering users to design the interface of their shopping app. With controls and options, users can craft dynamic layouts for their brand. The dashboard ensures seamless synchronization between user edits and real-time previews.',
+		languages: [icons.react, icons.mui, icons.redux],
+		image: 'waiter-dashboard.png',
+		link: 'https://waiter-dashboard.vercel.app/appBuilder',
+		gitLink: 'https://github.com/HossamMahmoudkhedr/waiter-dashboard',
+		done: true,
+	},
+	{
 		id: 12,
 		name: 'Waiter',
 		describtion:
-			"Created React and Material-UI-based website is a sophisticated platform featuring three dynamic landing pages dedicated to showcasing a state-of-the-art shopping app builder service. Integrating Material-UI's rich component library with React's robust architecture.",
+			"The React and Material-UI-based website is a sophisticated platform featuring three dynamic landing pages dedicated to showcasing a state-of-the-art shopping app builder service. Integrating Material-UI's rich component library with React's robust architecture.",
 		languages: [icons.react, icons.mui, icons.css],
 		image: 'waiter.png',
 		link: 'https://waiter-one.vercel.app/',
