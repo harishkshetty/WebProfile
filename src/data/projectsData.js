83f13130515aca09993e6a9a7fdda69198eab3c7
HossamMahmoudKhedr <nhem318@gmail.com>
2024-03-05 11:39:03 +0200

    add new projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -2,17 +2,6 @@ import { icons } from '../utils/icons';
 
 export const projData = [
 	{
-		id: 13,
-		name: 'Waiter Dashboard',
-		describtion:
-			'The Dashboard is an innovative platform empowering users to design the interface of their shopping app. With controls and options, users can craft dynamic layouts for their brand. The dashboard ensures seamless synchronization between user edits and real-time previews.',
-		languages: [icons.react, icons.mui, icons.redux],
-		image: 'waiter-dashboard.png',
-		link: 'https://waiter-dashboard.vercel.app/appBuilder',
-		gitLink: 'https://github.com/HossamMahmoudkhedr/waiter-dashboard',
-		done: true,
-	},
-	{
 		id: 12,
 		name: 'Waiter',
 		describtion:
@@ -21,6 +10,17 @@ export const projData = [
 		image: 'waiter.png',
 		link: 'https://waiter-one.vercel.app/',
 		gitLink: 'https://github.com/HossamMahmoudkhedr/waiter',
+		done: true,
+	},
+	{
+		id: 13,
+		name: 'Waiter Dashboard',
+		describtion:
+			'The Dashboard is an innovative platform empowering users to design the interface of their shopping app. With controls and options, users can craft dynamic layouts for their brand. The dashboard ensures seamless synchronization between user edits and real-time previews.',
+		languages: [icons.react, icons.mui, icons.redux],
+		image: 'waiter-dashboard.png',
+		link: 'https://waiter-dashboard.vercel.app/appBuilder',
+		gitLink: 'https://github.com/HossamMahmoudkhedr/waiter-dashboard',
 		done: true,
 	},
 	{
