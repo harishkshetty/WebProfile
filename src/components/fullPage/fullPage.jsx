7ac432c879224b5417bbfb5d70b014b78e696dc3
harish.s2 <harish.s@circles.asia>
2024-09-13 13:57:16 +0800

    enhanced the UI

--- src/components/fullPage/fullPage.jsx
+++ src/components/fullPage/fullPage.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Box } from '@mui/material';
 import Home from '../Home/home';
 import About from '../about/about';
-import Services from '../services/services';
 import Exed from '../exed/exed';
 import Work from '../work/work';
 import Skills from '../skills/skills';
@@ -12,7 +11,6 @@ const FullPage = () => {
 		<Box className="fullcontainer">
 			<Home />
 			<About />
-			<Services />
 			<Exed />
 			<Work />
 			<Skills />
